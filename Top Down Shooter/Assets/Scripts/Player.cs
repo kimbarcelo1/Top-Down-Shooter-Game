ac6d6cf0d37e875044b3ac4efb2dffe7ce23c8cf
Kim Barcelo <kim.barcelo@nbs.systems>
2021-02-13 11:08:03 +0800

    Initial Commit of Top Down Shooter Game

---
+++ Top Down Shooter/Assets/Scripts/Player.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float speed;
+    private Rigidbody2D rb2d;
+    private Animator anim;
+    private Vector2 moveAmount;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 moveInput = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+        moveAmount = moveInput.normalized * speed;
+
+        // chack if player is moving
+        if(moveInput != Vector2.zero)
+        {
+            anim.SetBool("isRunning", true);
+        }
+        else
+        {
+            anim.SetBool("isRunning", false);
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        rb2d.MovePosition(rb2d.position + moveAmount * Time.fixedDeltaTime);
+    }
+}
